61cf02b5fd968db91edbf1d99b7f8b69a4605c9c
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-06-18 21:50:27 +0100

    started filling out the home page (projects)

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -5,13 +5,44 @@ from dotenv import load_dotenv
 app = Flask(__name__)
 load_dotenv()
 
+projects = [
+    {
+        "name": "Drums",
+        "thumb": "images/drum-wee.png",
+        "hero": "images/drum-hero.png",
+        "categories": ["HTML", "CSS", "JS"],
+        "slug": "drums",
+        "production": "https://www.bbc.co.uk",
+    },
+    {
+        "name": "Dice",
+        "thumb": "images/dice-wee.png",
+        "hero": "images/dice-hero.png",
+        "categories": ["HTML", "CSS", "JS"],
+        "slug": "dice",
+        "production": "https://www.bbc.co.uk",
+    },
+    {
+        "name": "Memory",
+        "thumb": "images/memory-wee.png",
+        "hero": "images/memory-hero.png",
+        "categories": ["HTML", "CSS", "JS"],
+        "slug": "dice",
+        "production": "https://www.bbc.co.uk",
+
+    },
+]
+
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", projects=projects)
+
 
 @app.route("/about/")
 def about():
     return render_template("about.html")
+
 
 @app.route("/contact/")
 def contact():
